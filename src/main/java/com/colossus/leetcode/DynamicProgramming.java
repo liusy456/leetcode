7240f277e6659e1d43c11542a2bf260db57bfe2d
liusy <liusy456@gmail.com>
2020-04-01 20:58:25 +0800

    update: 修改结构

--- src/main/java/com/colossus/leetcode/DynamicProgramming.java
+++ src/main/java/com/colossus/leetcode/DynamicProgramming.java
@@ -147,6 +147,29 @@ public class DynamicProgramming {
         return dp[n];
     }
 
+    /**
+     * 按摩师
+     * @param nums
+     * @return
+     */
+    public int massage(int[] nums) {
+        int length = nums.length;
+        if(length<=0){
+            return 0;
+        }
+        if(length==1){
+            return nums[0];
+        }
+        int[] dp  = new int[length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for (int i = 2;i<length;i++){
+            //在今天是不是干活里选一个最优值
+            dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[length-1];
+    }
+
     public static void main(String[] args) {
         DynamicProgramming dp = new DynamicProgramming();
         int[][] grid = {{1,3,1},{1,5,1},{4,2,1}};
--- src/main/java/com/colossus/leetcode/TwoPointer.java
+++ src/main/java/com/colossus/leetcode/TwoPointer.java
@@ -4,7 +4,51 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ThreeSum {
+public class TwoPointer {
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+    public static ListNode addTwoNumbers(ListNode node1, ListNode node2) {
+
+        return add(node1,node2,0);
+    }
+
+    public static ListNode add(ListNode node1, ListNode node2,int c){
+        int v1 = 0;
+        int v2 = 0;
+        ListNode next1 = null;
+        ListNode next2 = null;
+        if(node1==null&&node2==null&&c==0){
+            return null;
+        }
+        if(node1!=null){
+            v1 = node1.val;
+            next1 = node1.next;
+        }
+        if(node2!=null){
+            v2 = node2.val;
+            next2 = node2.next;
+        }
+        int v = (v1+v2+c)%10;
+        //进位
+        int c1 = (v1+v2+c)/10;
+        ListNode result = new ListNode(v);
+        result.next = add(next1,next2,c1);
+        return result;
+    }
+    /**
+     * 最接近三数之和 lc-16
+     * @param nums
+     * @param target
+     * @return
+     */
     public int threeSumClosest(int[] nums, int target) {
         int min = Integer.MAX_VALUE;
         int result = 0;
@@ -32,6 +76,11 @@ public class ThreeSum {
         return result;
     }
 
+    /**
+     * 三数之和  lc-15
+     * @param nums
+     * @return
+     */
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
@@ -66,10 +115,27 @@ public class ThreeSum {
         return result;
     }
 
-    public static void main(String[] args) {
-        ThreeSum threeSum = new ThreeSum();
-        int[] nums={-1,2,1,-4};
-        int target = 1;
-        System.out.println(threeSum.threeSumClosest(nums,target));
+    /**
+     * 称最多水的容器 lc-11
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+        int maxArea = 0;
+        while (left!=right){
+            int L = height[left];
+            int R = height[right];
+            if(L<R){
+                maxArea = Math.max(maxArea,L*(right-left));
+                left++;
+            }else {
+                maxArea = Math.max(maxArea,R*(right-left));
+                right--;
+            }
+        }
+        return maxArea;
     }
+
 }
